4b3f972ff8c9ae0bbbbb890303cc1e0b542b4aaf
NithinAIML <nithin.aiml@gmail.com>
2024-05-10 03:37:50 +0530

    Comments Updated

--- Extraction.py
+++ Extraction.py
@@ -3,7 +3,9 @@ from  PyPDF2 import PdfReader
 from PIL import Image
 import re
 
-
+""" Method extract_text_from_pdf works on the basis of pyPDF2, which reads pdf and performs text extraction on it, Once the text is extracted
+    from the image, I have performed an operation where I iterate on each rows based on the lookup terms on each row, once I get retrieved
+    rows then performed parition to based on the lookup terms and selected the desired output """
 
 def extract_text_from_pdf(pdf_path, look_up_terms):
     text = ""
@@ -22,6 +24,11 @@ def extract_text_from_pdf(pdf_path, look_up_terms):
     return content
 
 
+""" Method extract_text_from_image works on pytesseract ocr, which will extract text from images, here I have used regex pattern and iterated text
+    on text of each row by using findall method and then considering the very first from all the rows which matched the pattern and giving
+    as a desired output """
+
+
 def extract_text_from_img(img_path):
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
     text = pytesseract.image_to_string(Image.open(img_path))
@@ -38,10 +45,14 @@ def extract_text_from_img(img_path):
                 result += out
     return result
 
+""" Look up terms for PDF and calling extract_text_from_pdf function """
+
 look_up_terms = ['positive', 'negative']
 pdf_text = extract_text_from_pdf("Data\\Breast-IHC-report-sample.pdf", look_up_terms=look_up_terms)
 print("Output from PDF:")
 print(pdf_text)
+
+""" Calling extract_text_from_img function """
 
 jpg_text = extract_text_from_img("Data\\biomarker_result.jpg")
 print("\nOutput from Image:")
